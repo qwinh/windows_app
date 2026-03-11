16211acdd960b81a71b8d6e23e4815b0cc8212c6
HuyHoang8324 <nguyenhuyhoangtpvl@gmail.com>
2026-03-11 21:04:59 +0700

    fix database

--- Helpers/clsDatbase.cs
+++ Helpers/clsDatbase.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace LibraryManagement.Helpers
 {
-    internal class clsDatbase
+    class clsDatabase
     {
+        public static SqlConnection con;
+
+        public static bool OpenConnection()
+        {
+            try
+            {
+                con = new SqlConnection(
+                    "Server=DESKTOP-LQE3B7F\\SQLEXPRESS;" +
+                    "Database=library;" +
+                    "Integrated Security=True;");
+                con.Open();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CloseConnection()
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
